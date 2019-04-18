222642a75d83440a2e09b25c239d02316119938a
Artem Kuzvesov <arktuz@gmail.com>
2019-04-18 23:51:04 +0500

    init

---
+++ src/app/speaker/speaker.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { SpeakersService } from '../speakers.service';
+
+@Component({
+  selector: 'app-speaker',
+  templateUrl: './speaker.page.html',
+  styleUrls: ['./speaker.page.scss'],
+})
+export class SpeakerPage implements OnInit {
+  public speaker: {
+    avatar: string;
+    company: string;
+    description: string;
+    name: string;
+    section: string;
+    title: string;
+  };
+
+  constructor(private activatedRoute: ActivatedRoute,
+              public speakersService: SpeakersService) { }
+
+  ngOnInit() {
+    this.activatedRoute
+      .params.subscribe(params => {
+        this.speaker = this.speakersService.get(params.id);
+    });
+  }
+}
